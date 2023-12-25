a35436fb25ebbc6b3fa0a49a75ea491578ab66ab
Max-Felix Müller <mxfxmmueller@gmail.com>
2023-12-25 13:30:13 +0100

    use left mouse button on drag

--- Whack_a_Monty.py
+++ Whack_a_Monty.py
@@ -22,4 +22,4 @@ def update():
         montyPosition = localToGlobalPosition(boundCenter, mainScreen=False)
 
         pyautogui.moveTo(montyPosition[0], montyPosition[1], _pause=False)
-        pyautogui.drag(0, 1, 0.11, _pause=False)
+        pyautogui.drag(0, 1, 0.11, _pause=False, button='left')
