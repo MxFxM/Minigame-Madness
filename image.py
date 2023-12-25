d811d65377b9412c2da1180202f0b161656dde21
Max-Felix Müller <mxfxmmueller@gmail.com>
2023-12-25 14:52:09 +0100

    comment out unused code when scale is 1

--- image.py
+++ image.py
@@ -11,12 +11,12 @@ def getScreenshot(mainScreen=True) -> Image:
     # Takes a screenshot based on which screen is required. 
     screenshot = pyautogui.screenshot(
         region=properties.mainScreen if mainScreen else properties.subScreen)
-    size = screenshot.size
-    scale = properties.mainScreenScale if mainScreen else\
-        properties.subScreenScale
+    #size = screenshot.size
+    #scale = properties.mainScreenScale if mainScreen else\
+    #    properties.subScreenScale
     # Scales image to match original DS screen size. 
-    screenshot = screenshot.resize( ( int(size[0] / scale), 
-        int(size[1] / scale) ), Image.NEAREST)
+    #screenshot = screenshot.resize( ( int(size[0] / scale), 
+    #    int(size[1] / scale) ), Image.NEAREST)
     return screenshot
 def localToGlobalPosition(position, mainScreen=True) -> tuple:
     # Converts a position on the subscreen to a position on the total screen. 
